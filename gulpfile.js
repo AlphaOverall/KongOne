cef2e4f22428e4e1b76b0d4d5cf7cf0b200464c9
resterman <resterman@zoho.com>
2016-11-10 15:14:52 -0300

    Adds userscript header comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
 const gulp      = require('gulp'),
       include   = require('gulp-include'),
       rename    = require('gulp-rename'),
-      uglifyjs  = require('uglify-js'),
       gutil     = require('gulp-util'),
       uglify    = require('gulp-uglify'),
-      minifier  = require('gulp-uglify/minifier'),
-      es6tr     = require('gulp-es6-transpiler'),
       jshint    = require('gulp-jshint'),
       babel     = require('gulp-babel'),
+      concat    = require('gulp-concat'),
       del       = require('del'),
       pump      = require('pump')
 
@@ -31,16 +29,6 @@ gulp.task('build', ['clean', 'lint'], function () {
 
 gulp.task('es6', ['build'], function () {
   return gulp.src('bin/kongOne.user.js')
-    // .pipe(es6tr({
-    //   environments: ['browser', 'devel', 'prototypejs'],
-    //   globals: {
-    //     unsafeWindow: false,
-    //     GM_getValue: true,
-    //     GM_setValue: true,
-    //     GM_deleteValue: true,
-    //     holodeck: false
-    //   }
-    // }))
     .pipe(babel({presets: ['es2015']}))
     .pipe(gulp.dest('bin'));
 });
@@ -51,9 +39,15 @@ gulp.task('uglify', ['build', 'es6'], function () {
     .pipe(gulp.dest('bin'));
 });
 
+gulp.task('userscript', ['uglify'], function () {
+  return gulp.src(['src/userscriptDefinition.js', 'bin/kongOne.user.js'])
+    .pipe(concat('kongOne.user.js'))
+    .pipe(gulp.dest('bin'))
+});
+
 gulp.task('watch', function() {
   gulp.watch('src/userscripts/*.js', ['build']);
 });
 
 
-gulp.task('default', ['clean', 'lint', 'build', 'es6','uglify']);
+gulp.task('default', ['clean', 'lint', 'build', 'es6','uglify', 'userscript']);
---
+++ src/userscriptDefinition.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name             Kongregate One
+// @namespace        profusiongames.com
+// @author           UnknownGuardian, AlphaOverall
+// @version          2.0.0
+// @date             04/19/2013
+// @include          http://www.kongregate.com/games/*/*
+// @include          http://www.kongregate.com/accounts/*
+// @description      Kongregate One - One script to rule them all. Everything here.
+// ==/UserScript==

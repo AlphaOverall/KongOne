d9ac8a86d507afaf2c923015b194640ceb454b33
Ruudiluca <ruudiluca@gmail.com>
2016-11-11 07:11:31 +0000

    Run the userscript on every page

--- src/userscriptDefinition.js
+++ src/userscriptDefinition.js
@@ -4,7 +4,6 @@
 // @author           UnknownGuardian, AlphaOverall
 // @version          2.0.0
 // @date             04/19/2013
-// @include          http://www.kongregate.com/games/*/*
-// @include          http://www.kongregate.com/accounts/*
+// @include          *://www.kongregate.com/*
 // @description      Kongregate One - One script to rule them all. Everything here.
 // ==/UserScript==
